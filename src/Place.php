291ba4d3c0b1d72ef9b28e13e2b0518b2ee6f6be
Darren Brown <dbrown@live.nl>
2021-10-06 23:15:51 +0200

    Extract place to separate Place class

---
+++ src/Place.php
@@ -0,0 +1,30 @@
+<?php
+
+class Place
+{
+    private int $position;
+
+    public function __construct()
+    {
+        $this->position = 0;
+    }
+
+    public function moveBy($steps): void
+    {
+        $this->position += $steps;
+
+        if ($this->position > 11) {
+            $this->moveBy(-12);
+        }
+    }
+
+    public function get(): int
+    {
+        return $this->position;
+    }
+
+    public function __toString()
+    {
+        return (string)$this->get();
+    }
+}
